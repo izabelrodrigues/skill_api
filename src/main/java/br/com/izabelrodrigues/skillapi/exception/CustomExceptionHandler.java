27634349e8208fbe05ef5ce6667f01128a768564
Izabel Rodrigues <izabel.castro.rodrigues@gmail.com>
2018-06-09 15:12:59 -0300

    Customizado nome dos objetos de erro e adicionado método para recuperar todos os erros de constraint violantion produzidos pelo validate

--- src/main/java/br/com/izabelrodrigues/skillapi/exception/CustomExceptionHandler.java
+++ src/main/java/br/com/izabelrodrigues/skillapi/exception/CustomExceptionHandler.java
@@ -1,5 +1,14 @@
 package br.com.izabelrodrigues.skillapi.exception;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.VndErrors;
 import org.springframework.hateoas.VndErrors.VndError;
 import org.springframework.http.HttpStatus;
@@ -8,14 +17,31 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import br.com.izabelrodrigues.skillapi.component.Mensagem;
+
 public class CustomExceptionHandler {
+
+	@Autowired
+	private Mensagem mensagem;
+
+	@Autowired
+	private Validator validator;
 
 	@ResponseBody
 	@ExceptionHandler(NotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	VndErrors notFoundHandler(NotFoundException ex) {
-		VndErrors vndErrors = new VndErrors("mensagem", ex.getMessage());
-		vndErrors.add(new VndError("httpStatusCode", HttpStatus.NOT_FOUND.toString()));
+		VndErrors vndErrors = new VndErrors("message", ex.getMessage());
+		vndErrors.add(new VndError("http_status", HttpStatus.NOT_FOUND.toString()));
+		return vndErrors;
+	}
+
+	@ResponseBody
+	@ExceptionHandler(ConstraintViolationException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	VndErrors constraintErrorHandler(ConstraintViolationException ex) {
+		VndErrors vndErrors = new VndErrors("message", ex.getMessage());
+		vndErrors.add(new VndError("http_status", HttpStatus.BAD_REQUEST.toString()));
 		return vndErrors;
 	}
 
@@ -23,8 +49,8 @@ public class CustomExceptionHandler {
 	@ExceptionHandler(Exception.class)
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	VndErrors internalServerErrorHandler(Exception ex) {
-		VndErrors vndErrors = new VndErrors("mensagem", ex.getMessage());
-		vndErrors.add(new VndError("httpStatusCode", HttpStatus.INTERNAL_SERVER_ERROR.toString()));
+		VndErrors vndErrors = new VndErrors("message", ex.getMessage());
+		vndErrors.add(new VndError("http_status", HttpStatus.INTERNAL_SERVER_ERROR.toString()));
 		return vndErrors;
 	}
 
@@ -32,9 +58,29 @@ public class CustomExceptionHandler {
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	VndErrors invalidArgurmentHandler(MethodArgumentNotValidException ex) {
-		VndErrors vndErrors = new VndErrors("mensagem", ex.getMessage());
-		vndErrors.add(new VndError("httpStatusCode", HttpStatus.BAD_REQUEST.toString()));
+		VndErrors vndErrors = new VndErrors("message", ex.getMessage());
+		vndErrors.add(new VndError("http_status", HttpStatus.BAD_REQUEST.toString()));
 		return vndErrors;
+	}
+
+	/**
+	 * Recupera a lista de erros ocasionados pelo Validate.
+	 * Os erros são de acordo com o idioma informado na requisição.
+	 * @param obj - Entidade a ser validada
+	 * @param idioma - Idioma do usuário recebido na requisição
+	 * @return
+	 */
+	protected String recuperaListaDeErros(Object obj,String idioma) {
+
+		Set<ConstraintViolation<Object>> constraintViolations = validator.validate(obj);
+
+		List<String> errorMessages = new ArrayList<>();
+
+		for (ConstraintViolation<Object> constraintViolation : constraintViolations) {
+			errorMessages.add(mensagem.getString(constraintViolation.getMessage(), idioma));
+		}
+		return errorMessages.toString();
+
 	}
 
 }
