5f8b9f1f044a8bdc75f180092efedc58fe0d86b2
Izabel Rodrigues <izabel.castro.rodrigues@gmail.com>
2018-06-09 15:14:53 -0300

    Adicionado anotaçao de HTTP_STATUS_CONFLICT para DuplicatedViolationException

--- src/main/java/br/com/izabelrodrigues/skillapi/exception/DuplicatedViolationException.java
+++ src/main/java/br/com/izabelrodrigues/skillapi/exception/DuplicatedViolationException.java
@@ -1,5 +1,9 @@
 package br.com.izabelrodrigues.skillapi.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class DuplicatedViolationException  extends RuntimeException{
 
 	/**
